5dd640ad7f35057ffa27774d3bba1c6d5ef67a26
Adam Blake <adam.blake@brainlabsdigital.com>
2026-07-20 16:13:23 -0300

    added reviewers and commet models

--- cafes/admin.py
+++ cafes/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Cafe, Barrio, Review
+from .models import Cafe, Barrio, Comment, Reviewer
 
 
 # Register your models here.
 admin.site.register(Cafe)
 admin.site.register(Barrio)
-admin.site.register(Review)
+admin.site.register(Comment)
+admin.site.register(Reviewer)
--- cafes/serializers.py
+++ cafes/serializers.py
@@ -1,27 +1,31 @@
 from rest_framework import serializers
-from .models import Cafe, Barrio, Review
+from .models import Cafe, Barrio, Comment, Reviewer
 
-
-class ReviewSerializer(serializers.ModelSerializer):
+class ReviewerSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Review
+        model = Reviewer
+        fields = ['name', 'join_date']
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
         fields = ['cafe', 'comment', 'reviewer_name']
 
     # We create a class that inherits from DRF's ModelSerializer.
 class CafeSerializer(serializers.ModelSerializer):
 
     number_of_reviews = serializers.SerializerMethodField()
-    reviews = ReviewSerializer(many=True, read_only=True, source="review_set")
+    reviews = CommentSerializer(many=True, read_only=True, source="review_set")
     summary = serializers.SerializerMethodField()
         # The Meta class is where we configure the serializer.
     class Meta:
             # 1. Tell the serializer which model it's based on.
         model = Cafe
             # 2. Define the "whitelist" of fields to include in the API.
-        fields = ['id', 'name', 'barrio', 'address', 'rating', 'has_good_medialunas', 'summary', 'number_of_reviews', 'reviews']
+        fields = ['id', 'name', 'barrio', 'address', 'rating', 'has_good_medialunas', 'summary', 'number_of_reviews', 'reviews', 'notes']
     
     def get_barrio(self, obj):
-        return obj.barrio.value
+        return obj.barrio.label
     
     def get_summary(self, obj):
         return f"{obj.name} is a {obj.rating}-star cafe."
